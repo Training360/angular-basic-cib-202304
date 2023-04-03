9d033bed48048b8d95edb3253e3e145e90a321e0
cherryApp <j.cserko@gmail.com>
2023-04-03 14:38:18 +0200

    component input

---
+++ angular-components/src/app/page/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  @Input() componentTitle: string = 'Product Manager';
+}
