c2d35133c3699d42a9a5ae8576279417f309d724
cherryApp <j.cserko@gmail.com>
2023-04-04 14:39:25 +0200

    routing

--- routing-http/src/app/app.module.ts
+++ routing-http/src/app/app.module.ts
@@ -6,11 +6,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './page/user/user.component';
+import { HomeComponent } from './page/home/home.component';
+import { UserEditComponent } from './page/user-edit/user-edit.component';
+import { NavComponent } from './common/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    HomeComponent,
+    UserEditComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
