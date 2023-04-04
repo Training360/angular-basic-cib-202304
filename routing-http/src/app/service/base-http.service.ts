0707d86079791c3cff43cdfc633c30388f2d3ed5
cherryApp <j.cserko@gmail.com>
2023-04-04 14:13:00 +0200

    crud

--- routing-http/src/app/service/base-http.service.ts
+++ routing-http/src/app/service/base-http.service.ts
@@ -17,6 +17,24 @@ export abstract class BaseHttpService<T extends {id: number}> {
     return this.http.get<T[]>(`${this.apiUrl}${this.entityName}`);
   }
 
+  get(id: number): Observable<T> {
+    return this.http.get<T>(`${this.apiUrl}${this.entityName}/${id}`);
+  }
+
+  create(entity: T): Observable<T> {
+    return this.http.post<T>(
+      `${this.apiUrl}${this.entityName}`,
+      entity
+    );
+  }
+
+  update(entity: T): Observable<T> {
+    return this.http.patch<T>(
+      `${this.apiUrl}${this.entityName}/${entity.id}`,
+      entity
+    );
+  }
+
   delete(entity: T): Observable<T> {
     return this.http.delete<T>(`${this.apiUrl}${this.entityName}/${entity.id}`);
   }
