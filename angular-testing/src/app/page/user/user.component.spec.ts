b98f9719fc3661a05f0bbc2158ab5916bde60916
cherryApp <j.cserko@gmail.com>
2023-04-24 09:46:41 +0200

    mocking services

--- angular-testing/src/app/page/user/user.component.spec.ts
+++ angular-testing/src/app/page/user/user.component.spec.ts
@@ -7,6 +7,15 @@ import { DataTableComponent } from 'src/app/common/data-table/data-table.compone
 import { OrderPipe } from 'src/app/pipe/order.pipe';
 import { FilterPipe } from 'src/app/pipe/filter.pipe';
 import { FormsModule } from '@angular/forms';
+import { UserService } from 'src/app/service/user.service';
+import { Observable, of } from 'rxjs';
+import { User } from 'src/app/model/user';
+
+const mockUsers: User[] = [
+  { id: 1, name: 'Iron', email: 'test@gmail.com', category: 'Admin', bio: '' },
+  { id: 2, name: 'Bread', email: 'test@gmail.com', category: 'Admin', bio: '' },
+  { id: 3, name: 'Lemon', email: 'test@gmail.com', category: 'Admin', bio: '' },
+];
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -14,12 +23,27 @@ describe('UserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserComponent, DataTableComponent, OrderPipe, FilterPipe ],
+      declarations: [ 
+        UserComponent, 
+        DataTableComponent, 
+        OrderPipe, 
+        FilterPipe 
+      ],
       imports: [
         RouterTestingModule,
         HttpClientTestingModule,
         FormsModule,
       ],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            getAll: (): Observable<User[]> => {
+              return of(mockUsers);
+            }
+          }
+        }
+      ],    
     })
     .compileComponents();
 
@@ -30,5 +54,29 @@ describe('UserComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  
+  it('users table should show', () => {
+    const table = fixture.nativeElement.querySelector('table.table');
+
+    expect(table).toBeTruthy();
+  });
+  
+  it('table rows should exist', () => {
+    const tableRows = fixture.nativeElement.querySelectorAll('table.table tbody tr');
+
+    expect(tableRows.length).toEqual(3);
+  });
+  
+  it('user id should be correct', () => {
+    const tableRows = fixture.nativeElement.querySelectorAll('table.table tbody tr');
+
+    expect(tableRows[0].querySelector('td').textContent).toMatch(/1/);
+  });
+  
+  it('user name should be correct', () => {
+    const tableRows = fixture.nativeElement.querySelectorAll('table.table tbody tr');
+
+    expect(tableRows[0].querySelector('td:nth-child(2)').textContent).toMatch(/Iron/);
   });
 });
